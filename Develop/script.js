e80504b11a2db5205ac942cfb968865c6e49f522
Dylan McAulay <dmcaulay97@gmail.com>
2021-04-22 20:48:59 -0400

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
+var timeDisplay = $(".timeDisplay");
 var container = $(".container");
-var currentTime = moment();
-var currentHour = parseInt(currentTime.format("HH"));
-console.log(currentHour);
+var Time = moment();
+var currentHour = parseInt(Time.format("HH"));
 
 for (var i = 9; i < 18; i++) {
     var row = $("<div></div>").attr("class", "row");
@@ -13,25 +13,34 @@ for (var i = 9; i < 18; i++) {
         var timeLabel = (i - 12) + "PM";
     }
 
-    hour.append($("<div></div>").text(timeLabel));
+    hour.append($("<div class = 'margin'></div>").text(timeLabel));
     row.append(hour);
     var text = $("<div></div>");
-    console.log("Hello!!");
-    text.attr("class", "past col-8");
-    // if (i < currentHour) {
-    //     text.attr("class", "past col-8");
-    // } else if (i = currentHour) {
-    //     text.attr("class", "present col-8");
-    // } else {
-    //     text.attr("class", "future col-8");
-    // }
+    if (i < currentHour) {
+        text.attr("class", "past col-8");
+    } else if (i == currentHour) {
+        text.attr("class", "present col-8");
+    } else {
+        text.attr("class", "future col-8");
+    }
 
-    var textArea = $("<div></div>").attr("class", "text");
+    var textArea = $("<div></div>").attr("class", "text margin");
     textArea.attr("contenteditable", "true");
     text.append(textArea);
     row.append(text);
-    row.append($("<div></div>").attr("class", "saveBtn col-2"));
+    row.append($("<div><div class = 'margin'>🔒</div></div>").attr("class", "saveBtn col-2"));
     container.append(row);
 
-
+    var save = $(".saveBtn");
+    save.on("click", function (e) {
+        var btn = e.target;
+    });
 }
+
+var inter = setInterval(function () {
+    var currentTime = moment();
+    currentTime = currentTime.format("MMMM Do, YYYY, hh:mm:ss");
+    timeDisplay.text(currentTime);
+}, 1000);
+
+
